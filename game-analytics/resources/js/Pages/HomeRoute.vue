c416642b8c7a2182e811c26cae66eaea8062283b
Mkhanyisi Gamedze <mkhanyisi.g@gmail.com>
2024-08-09 01:23:25 -0400

    setup vue frontend

--- game-analytics/resources/js/Pages/HomeRoute.vue
+++ game-analytics/resources/js/Pages/HomeRoute.vue
@@ -1,25 +1,56 @@
 <template>
     <div>
         <h1>Mkhanyisi Gamefam test App </h1>
-        <router-link to="/test"> Take me to Test page </router-link>
-        <button @click.prevent="tiggerEndpoint">Trigger Endpoint</button>
-        <p v-if="response">{{ response.data }}</p>
+        <h2>Gamefam Analytics</h2>
+        <h2>Live Online Users: {{ liveCount }}</h2>
+        <chartkick :data="chartData" library="highcharts"></chartkick>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Peak Users</th>
+                    <th>Average Users</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in tableData" :key="row.date">
+                    <td>{{ row.date }}</td>
+                    <td>{{ row.peak_users }}</td>
+                    <td>{{ row.average_users }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script setup>
-    import axios from "axios";
-    import { ref } from "vue";
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
-    const response = ref();
+const liveCount = ref(0);
+const chartData = ref([]);
+const tableData = ref([]);
 
-    const getValue = async () => {
-        try {
-            response.value = await axios.get("/api/test-me");
-        } catch (error) {
-            // Do something with the error
-            console.log(error);
-        }
-    };
+const fetchLiveCount = async () => {
+    const response = await axios.get('/api/online-users/live');
+    liveCount.value = response.data.count;
+};
 
-<script>
+const fetchChartData = async () => {
+    const response = await axios.get('/api/online-users/chart');
+    chartData.value = response.data.map(item => [item.retrieved_at, item.count]);
+};
+
+const fetchTableData = async () => {
+    const response = await axios.get('/api/online-users/table');
+    tableData.value = response.data;
+};
+
+onMounted(() => {
+    fetchLiveCount();
+    fetchChartData();
+    fetchTableData();
+
+    setInterval(fetchLiveCount, 60000); // Update live count every minute
+});
+</script>
