2b2c5276593b78ce07e41790fe0933c0031443b9
Mkhanyisi Gamedze <mkhanyisi.g@gmail.com>
2024-08-20 04:11:02 -0400

    add new basic tests on online users table

--- game-analytics/tests/Feature/OnlineUsersTest.php
+++ game-analytics/tests/Feature/OnlineUsersTest.php
@@ -5,16 +5,72 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\OnlineUser; 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+
 
 class OnlineUsersTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    protected $onlineUser;
+
+    protected $DB ;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->onlineUser = new OnlineUser(); 
+        $this->DB = DB::table('online_users');
+    }
+
+    /**
+     * Test the insertion of a new record in the online_users table.
+     */
+    public function test_it_can_insert_new_record_using_database()
+    {
+        // Insert data into the online_users table
+        DB::table('online_users')->insert([
+            'count' => 7,
+            'retrieved_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Assertions to check the data
+        $this->assertDatabaseHas('online_users', ['count' => 7]);
+    }
+
+
+    /**
+     * Test the users:check command.
+     */
+    public function test_check_active_users_command(): void
+    {
+        {
+            Artisan::call('users:check');
+    
+            // Check the command output
+            $output = Artisan::output();
+
+            $this->assertStringContainsString('Active users count saved successfully', $output);
+            
+        }
+    }
+
+    /**
+     * Test the insertion of a new record in the online_users table.
+     */
+    public function test_create_new_model_record()
+    {
+        $data = [
+            'count' => 5,
+            'retrieved_at' => now(), // current timestamp
+        ];
+
+        $result = $this->onlineUser->create($data);
+
+        $this->assertInstanceOf(OnlineUser::class, $result); // Check that the insert operation was successful
     }
 }
